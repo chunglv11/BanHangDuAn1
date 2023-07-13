1d2f281b91448071da8fcf43c1ea6a20c2b7a530
tranbach241203 <117243743+tranbach241203@users.noreply.github.com>
2023-07-13 09:07:51 +0700

    tạm
    chức vụ + nhân viên

--- 1.DAL/IRepository/IChucVuResponsitory.cs
+++ 1.DAL/IRepository/IChucVuResponsitory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace _1.DAL.IRepository
 {
     public interface IChucVuResponsitory
     {
+        string add(ChucVu chucVu);
+        string update(ChucVu chucVu);
+        string delete(ChucVu chucVu);
+        public List<ChucVu> GetAll();
+
+
     }
 }
+
--- 1.DAL/Repository/ChucVuResponsitory.cs
+++ 1.DAL/Repository/ChucVuResponsitory.cs
@@ -1,4 +1,5 @@
 ﻿using _1.DAL.IRepository;
+using _1.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,48 @@ namespace _1.DAL.Repository
 {
     public class ChucVuResponsitory : IChucVuResponsitory
     {
+        ShopContext _shopContext;
+        List<ChucVu> _lstchucvus;
+        public ChucVuResponsitory()
+        {
+            _shopContext = new ShopContext();
+            _lstchucvus = new List<ChucVu>();
+
+        }
+
+
+        public string add(ChucVu chucVu)
+        {
+            _shopContext.ChucVus.Add(chucVu);
+            _shopContext.SaveChanges();
+
+            return "Thêm dữ liệu thành công";
+        }
+
+        public string delete(ChucVu chucVu)
+        {
+            _shopContext.ChucVus.Remove(chucVu);
+            _shopContext.SaveChanges();
+
+            return "Xóa dữ liệu thành công";
+        }
+
+        public List<ChucVu> GetAll()
+        {
+            return _lstchucvus;
+        }
+
+
+
+
+
+        public string update(ChucVu chucvu)
+        {
+            _shopContext.ChucVus.Update(chucvu);
+            _shopContext.SaveChanges();
+
+            return "Cập nhật dữ liệu thành công";
+        }
+
     }
 }
