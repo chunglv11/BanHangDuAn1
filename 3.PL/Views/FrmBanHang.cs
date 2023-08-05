873c7e305cff8979a19ea636922ad94bd6ef3229
chunglv11 <info.chungnd@gmail.com>
2023-08-05 21:09:37 +0700

    fix lại tí

--- 3.PL/Views/FrmBanHang.cs
+++ 3.PL/Views/FrmBanHang.cs
@@ -203,10 +203,10 @@ namespace _3.PL.Views
                             var tongsp = data.SoLuong += Convert.ToInt32(content);
                             if (tongsp > p.SoLuongTon)
                             {
-                                DialogResult dialogResult = MessageBox.Show("bạn có muốn mua hết không", "Thông Báo", MessageBoxButtons.YesNo);
+                                DialogResult dialogResult = MessageBox.Show($"Số lượng mua được tối đa là: {p.SoLuongTon}", "Thông Báo", MessageBoxButtons.YesNo);
                                 if (dialogResult == DialogResult.Yes)
                                 {
-                                    MessageBox.Show($"Số lượng mua được tối đa là: {p.SoLuongTon}");
+                                    //MessageBox.Show($"Số lượng mua được tối đa là: {p.SoLuongTon}");
                                     data.SoLuong = p.SoLuongTon;
                                 }
                                 else
